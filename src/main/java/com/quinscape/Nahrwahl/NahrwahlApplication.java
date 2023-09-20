f99c86458fc54c11e4334558fb92c2aa43c2f7f3
Jakob Klimczok <jklimczok@veda.fritz.box>
2023-09-20 20:13:09 +0200

    first init

---
+++ src/main/java/com/quinscape/Nahrwahl/NahrwahlApplication.java
@@ -0,0 +1,13 @@
+package com.quinscape.Nahrwahl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahrwahlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NahrwahlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quinscape/Nahrwahl/NahrwahlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinscape.Nahrwahl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahrwahlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
